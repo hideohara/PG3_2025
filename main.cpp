07e88cc821d890cd497c6da58350696b1720ce53
ohara <ohara@g.neec.ac.jp>
2025-04-30 19:34:35 +0900

    2-2. クラステンプレート

--- main.cpp
+++ main.cpp
@@ -1,17 +1,28 @@
-#include <iostream>
-#include <windows.h>
+#include <stdio.h>
 
-int main()
-{
-	//SetConsoleOutputCP(65001);
-	char str[] = "ソ";
-	printf("%s",str);
-	return 0;
+template <typename T>
+class Min {
+public:
+    T num1;
+    T num2;
+
+    Min(T num1, T num2) : num1(num1), num2(num2){}
+    // ※ページ都合でheader内で定義している
+
+    T Size() {
+        if (num1 < num2)
+            return num1;
+        else
+            return num2;
+    }
+};
+
+int main() {
+    Min<int> minInt(10, 20);
+    Min<float> minFloat(1.2f, 3.4f);
+    Min<double> minDouble(1.2, 3.4);
+    printf("int 小さい値は%d\n", minInt.Size());
+    printf("float 小さい値は%f\n", minFloat.Size());
+    printf("double 小さい値は%lf\n", minDouble.Size());
+    return 0;
 }
-
-
-
-
-
-
-
