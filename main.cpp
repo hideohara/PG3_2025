02b22d2bb5a7c47ab7cf8973fb53590a47e0ba01
ohara <ohara@g.neec.ac.jp>
2025-05-08 17:14:20 +0900

    課題

--- main.cpp
+++ main.cpp
@@ -12,11 +12,22 @@ Type Min(Type num1, Type num2) {
 	}
 }
 
+template <>
+char Min<char>(char a, char b) {
+	printf("文字処理\n");
+	return ' ';
+}
+
+
 // メイン処理
 int main() {
 	// 整数で呼び出す
 	printf("小さい値は%d\n", Min<int>(114, 514));
 	// 小数で呼び出す
 	printf("小さい値は%f\n", Min<float>(11.4f, 51.4f));
+	// doubleで呼び出す
+	printf("小さい値は%f\n", Min<double>(11.4, 51.4));
+	// 文字で呼び出す
+	printf("%c\n", Min<char>('1', '2'));
 	return 0;
 }
