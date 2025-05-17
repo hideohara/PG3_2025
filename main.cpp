33db7c7c2df71ceb7f4567714931d47975ec8838
ohara <ohara@g.neec.ac.jp>
2025-05-17 11:34:17 +0900

    修正

--- main.cpp
+++ main.cpp
@@ -1,28 +1,34 @@
 #include <stdio.h>
 
-template <typename T>
+template <typename T1, typename T2>
 class Min {
 public:
-    T num1;
-    T num2;
+    T1 num1;
+    T2 num2;
 
-    Min(T num1, T num2) : num1(num1), num2(num2){}
+    Min(T1 num1, T2 num2) : num1(num1), num2(num2){}
     // ※ページ都合でheader内で定義している
 
-    T Size() {
+    T1 Size() {
         if (num1 < num2)
-            return num1;
+            return static_cast<T1>(num1);
         else
-            return num2;
+            return static_cast<T1>(num2);
     }
 };
 
 int main() {
-    Min<int> minInt(10, 20);
-    Min<float> minFloat(1.2f, 3.4f);
-    Min<double> minDouble(1.2, 3.4);
-    printf("int 小さい値は%d\n", minInt.Size());
-    printf("float 小さい値は%f\n", minFloat.Size());
-    printf("double 小さい値は%lf\n", minDouble.Size());
+    Min<int,int> minIntInt(10, 20);
+    Min<float, float> minFloatFloat(1.2f, 3.4f);
+    Min<double, double> minDoubleDouble(1.2, 3.4);
+    Min<int, float> minIntFloat(10, 20.5f);
+    Min<int, double> minIntDouble(10, 5.5);
+    Min<float, double> minFloatDouble(10.3f, 5.5);
+    printf("int, int 小さい値は%d\n", minIntInt.Size());
+    printf("float, float 小さい値は%f\n", minFloatFloat.Size());
+    printf("double, double 小さい値は%lf\n", minDoubleDouble.Size());
+    printf("int, float 小さい値は%d\n", minIntFloat.Size());
+    printf("int, double 小さい値は%d\n", minIntDouble.Size());
+    printf("float, double 小さい値は%f\n", minFloatDouble.Size());
     return 0;
 }
